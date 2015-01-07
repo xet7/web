9d5b6d8c7f610f78ac8fa870052d0c52e379cd6f
let4be <let4be@gmail.com>
2015-01-07 19:22:05 +0300

    openpgp cleanup, added localStorage persistance

--- src/js/apps/AppLavaboomLogin/services/openpgp.js
+++ src/js/apps/AppLavaboomLogin/services/openpgp.js
@@ -1,5 +1,5 @@
 angular.module('AppLavaboomLogin').service('openpgp', function($q){
-	var keys = {};
+	var keyring = localStorage['keyring'] ? JSON.parse(localStorage['keyring']) : {};
 
 	this.initialize = () => {
 		openpgp.initWorker('/vendor/openpgp.worker.js');
@@ -28,68 +28,18 @@ angular.module('AppLavaboomLogin').service('openpgp', function($q){
 
 		openpgp.generateKeyPair({numBits: numBits, userId: email, passphrase: password})
 			.then(freshKeys => {
-				keys[email] = freshKeys;
+				keyring[email] = {
+					key_pub: freshKeys.publicKeyArmored,
+					key_prv: freshKeys.privateKeyArmored,
+					generated: Date.now()
+				};
+				localStorage['keyring'] = JSON.stringify(keyring);
 
 				deferred.resolve(freshKeys);
 			})
 			.catch(error =>{
 				deferred.reject(error);
 			});
-
-		return deferred.promise;
-	};
-
-	this.generateKbpgpKeys = (email, password, numBits) => {
-		if (!numBits)
-			numBits = 1024;
-
-		var F = kbpgp["const"].openpgp;
-
-		var opts = {
-			userid: email,
-			primary: {
-				nbits: numBits,
-				flags: F.certify_keys | F.sign_data | F.auth | F.encrypt_comm | F.encrypt_storage,
-				expire_in: 0
-			},
-			subkeys: [
-			]
-		};
-
-		var deferred = $q.defer();
-
-		kbpgp.KeyManager.generate(opts, (error, alice) => {
-			if (error)
-				return deferred.reject(error);
-
-			var privateKey = null;
-			var publicKey = null;
-
-			alice.sign({}, err => {
-				console.log(alice);
-
-				alice.export_pgp_private_to_client({
-					passphrase: password
-				},  (err, pgp_private) => {
-					privateKey = pgp_private;
-
-					if (privateKey && publicKey)
-						deferred.resolve({
-							privateKeyArmored: privateKey,
-							publicKeyArmored: publicKey
-						});
-				});
-				alice.export_pgp_public({}, (err, pgp_public) =>{
-					publicKey = pgp_public;
-
-					if (privateKey && publicKey)
-						deferred.resolve({
-							privateKeyArmored: privateKey,
-							publicKeyArmored: publicKey
-						});
-				});
-			});
-		});
 
 		return deferred.promise;
 	};
