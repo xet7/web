f7e535c3f74fb3e424e58f2f64ebdd41f6be0d41
KseniyaKuprina <light.it.zp+kseniya@gmail.com>
2015-01-09 15:33:24 +0200

    add .jade files in the LogIn

--- src/js/apps/AppLavaboomLogin/controllers/ctrlAuth.js
+++ src/js/apps/AppLavaboomLogin/controllers/ctrlAuth.js
@@ -10,7 +10,10 @@ angular.module('AppLavaboomLogin').controller('AuthController', function($scope,
          "password": CryptoJS.SHA3($scope.auth.password, { outputLength: 256 }).toString()
          }).then(function (data) {
             console.log(data);
-            $location.path.url('/index.html');
+           // $location.absUrl("http://0.0.0.0:5000/");
+           /* $location.path("http://0.0.0.0:5000/index.html");
+            $location.replace();*/
+            window.location="http://0.0.0.0:5000/";
          }).catch(function (err) {
          console.log(err);
          });
--- src/js/apps/AppLavaboomLogin/controllers/ctrlVerify.js
+++ src/js/apps/AppLavaboomLogin/controllers/ctrlVerify.js
@@ -1,3 +1,26 @@
-/**
- * Created by user on 09.01.15.
- */
+angular.module('AppLavaboomLogin').controller('VerifyController', function($scope, openpgp, LavaboomAPI) {
+$scope.invite=[];
+
+    $scope.send=function() {
+        LavaboomAPI.accounts.reserve.invited({
+            "username": $scope.invite.username,
+            "password": CryptoJS.SHA3($scope.invite.password, {outputLength: 256}).toString(),
+            "token": $scope.invite.token
+        }).then(function (resp) {
+            console.log(resp);
+        }).catch(function (err) {
+            console.log(err);
+        });
+      /*  LavaboomAPI.accounts.create.invited({
+            "username": $scope.invite.username,
+            "password": CryptoJS.SHA3($scope.invite.password, {outputLength: 256}).toString(),
+            "token": $scope.invite.token
+        }).then(function (resp) {
+            console.log(resp);
+        }).catch(function (err) {
+            console.log(err);
+        });*/
+    };
+});
+
+//oiFcopQPcXsgjv3AiBgI
