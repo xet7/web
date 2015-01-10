97866b69977dfbe9309bd1504d7ccfc2ba420453
let4be <let4be@gmail.com>
2015-01-10 13:58:51 +0300

    gulpfile: removed unused dependencies and utilized plugins auto-loader

--- gulpfile.js
+++ gulpfile.js
@@ -2,57 +2,38 @@
 var fs = require('fs');
 var del = require('del');
 var path = require('path');
-var serve = require('./serve');
 var spawn = require('child_process').spawn;
 var toml = require('toml');
+var lazypipe = require('lazypipe');
 
 // General
 var gulp = require('gulp');
 var traceur = require('gulp-traceur');
-var lazypipe = require('lazypipe');
-var plumber = require('gulp-plumber');
-var flatten = require('gulp-flatten');
-var tap = require('gulp-tap');
-var rename = require('gulp-rename');
-var header = require('gulp-header');
-var footer = require('gulp-footer');
-var watch = require('gulp-watch');
-var package = require('./package.json');
 var plg = require('gulp-load-plugins')({
 	pattern: ['gulp-*', 'gulp.*'],
 	replaceString: /\bgulp[\-.]/
 });
 
-// Scripts and tests
-var ngAnnotate = require('gulp-ng-annotate');
-var jshint = require('gulp-jshint');
-var jshintStylish = require('jshint-stylish');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var karma = require('gulp-karma');
-var include = require('gulp-include');
-var less = require('gulp-less');
+// Modules
+var serve = require('./serve');
 
-// SVGs
-var svgmin = require('gulp-svgmin');
-var svgstore = require('gulp-svgstore');
-
-// Docs
-var markdown = require('gulp-markdown');
-var fileinclude = require('gulp-file-include');
-
-
+// Configuration
+var package = require('./package.json');
 var paths = require('./gulp/paths');
 var config = require('./gulp/config');
+
+// Global variables
+var childProcess = null;
+var args = process.argv.slice(2);
 var appsCache = {};
 var appsCacheMin = {};
 
-var childProcess = null;
-var args = process.argv.slice(2);
+
 
 /**
  * Gulp Taks
  */
+
 
 
 // process angular.js applications without their 3rd party dependencies
@@ -61,17 +42,17 @@ var args = process.argv.slice(2);
 
 gulp.task('build:apps', ['clean:dist', 'lint:scripts'], function() {
 	var prodPipeline = lazypipe()
-		.pipe(uglify)
-		.pipe(tap, function (file, t) {
+		.pipe(plg.uglify)
+		.pipe(plg.tap, function (file, t) {
 			appsCacheMin[file.relative] = file.contents;
 		});
 
 	return gulp.src(paths.scripts.inputApps)
 		.pipe(plg.cached('build:apps'))
-		.pipe(include())
+		.pipe(plg.include())
 		.pipe(traceur())
-		.pipe(ngAnnotate())
-		.pipe(tap(function (file, t) {
+		.pipe(plg.ngAnnotate())
+		.pipe(plg.tap(function (file, t) {
 			appsCache[file.relative] = file.contents;
 		}))
 		.pipe(config.isProduction ? prodPipeline() : plg.util.noop());
@@ -83,7 +64,7 @@ gulp.task('build:scripts', ['clean:dist', 'lint:scripts', 'build:apps', 'build:t
 		.pipe(gulp.dest, paths.scripts.output);
 
 	return gulp.src(paths.scripts.input)
-		.pipe(plumber())
+		.pipe(plg.plumber())
 		.pipe(plg.replace(/require "(.*).js"/g, function(match, p1, p2, p3, offset, string) {
 			var file = p1 + (config.isProduction ? '.min.js' : '.js');
 			var resolvedFile = path.resolve(__dirname, path.dirname(paths.scripts.input), file);
@@ -99,7 +80,7 @@ gulp.task('build:scripts', ['clean:dist', 'lint:scripts', 'build:apps', 'build:t
 
 			return 'console.error("Cannot find angular.js include \\"' + file + '\\"!")';
 		}))
-		.pipe(include())
+		.pipe(plg.include())
 		.pipe(plg.replace(/\/\/\s*=\s*require-application "(.*).js"/g, function(match, p1, p2, p3, offset, string) {
 			var key = p1 + '.js';
 			var cache = config.isProduction ? appsCacheMin : appsCache;
@@ -116,17 +97,17 @@ gulp.task('build:scripts', ['clean:dist', 'lint:scripts', 'build:apps', 'build:t
 // Lint scripts
 gulp.task('lint:scripts', function () {
 	return gulp.src(paths.scripts.inputAll)
-		.pipe(plumber())
+		.pipe(plg.plumber())
 		.pipe(plg.cached('lint:scripts'))
-		.pipe(tap(function(file, t){
-			console.log('linting: ', file.relative);
+		.pipe(plg.tap(function(file, t){
+			console.log('Linting: "' + file.relative + '" ...');
 		}))
-		.pipe(jshint({
+		.pipe(plg.jshint({
 			esnext: true,
 			noyield: true
 		}))
-		.pipe(jshint.reporter(jshintStylish))
-		.pipe(jshint.reporter('fail'));
+		.pipe(plg.jshint.reporter(plg.jshintStylish))
+		.pipe(plg.jshint.reporter('fail'));
 });
 
 // Process, lint, and minify less files
@@ -136,14 +117,14 @@ gulp.task('build:styles', ['clean:dist'], function() {
 			keepSpecialComments: 0
 		})
 		//.pipe(header, config.banner.min, { package : package })
-		.pipe(rename, { suffix: '.min' })
+		.pipe(plg.rename, { suffix: '.min' })
 		.pipe(gulp.dest, paths.styles.output)
 		.pipe(plg.gzip)
 		.pipe(gulp.dest, paths.styles.output);
 
 	return gulp.src(paths.styles.input)
-		.pipe(plumber())
-		.pipe(less())
+		.pipe(plg.plumber())
+		.pipe(plg.less())
 		.pipe(plg.autoprefixer('last 2 version', '> 1%'))
 		//.pipe(header(config.banner.full, { package : package }))
 		.pipe(gulp.dest(paths.styles.output))
@@ -153,13 +134,13 @@ gulp.task('build:styles', ['clean:dist'], function() {
 // Generate SVG sprites
 gulp.task('build:svgs', ['clean:dist'], function () {
 	return gulp.src(paths.svgs.input)
-		.pipe(plumber())
-		.pipe(tap(function (file, t) {
+		.pipe(plg.plumber())
+		.pipe(plg.tap(function (file, t) {
 			if ( file.isDirectory() ) {
 				var name = file.relative + '.svg';
 				return gulp.src(file.path + '/*.svg')
-					.pipe(svgmin())
-					.pipe(svgstore({
+					.pipe(plg.svgmin())
+					.pipe(plg.svgstore({
 						fileName: name,
 						prefix: 'icon-',
 						inlineSvg: true
@@ -167,8 +148,8 @@ gulp.task('build:svgs', ['clean:dist'], function () {
 					.pipe(gulp.dest(paths.svgs.output));
 			}
 		}))
-		.pipe(svgmin())
-		.pipe(svgstore({
+		.pipe(plg.svgmin())
+		.pipe(plg.svgstore({
 			fileName: 'icons.svg',
 			prefix: 'icon-',
 			inlineSvg: true
@@ -179,28 +160,28 @@ gulp.task('build:svgs', ['clean:dist'], function () {
 // Copy static files into output folder
 gulp.task('copy:vendor', ['clean:dist'], function() {
 	return gulp.src(paths.vendor.input)
-		.pipe(plumber())
+		.pipe(plg.plumber())
 		.pipe(gulp.dest(paths.vendor.output));
 });
 
 // Copy images into output folder
 gulp.task('copy:imgs', ['clean:dist'], function() {
 	return gulp.src(paths.img.input)
-		.pipe(plumber())
+		.pipe(plg.plumber())
 		.pipe(gulp.dest(paths.img.output));
 });
 
 // Copy fonts into output folder
 gulp.task('copy:fonts', ['clean:dist'], function() {
 	return gulp.src(paths.fonts.input)
-		.pipe(plumber())
+		.pipe(plg.plumber())
 		.pipe(gulp.dest(paths.fonts.output));
 });
 
 // Copy static files into output folder
 gulp.task('copy:static', ['clean:dist'], function() {
 	return gulp.src(paths.staticFiles)
-		.pipe(plumber())
+		.pipe(plg.plumber())
 		.pipe(gulp.dest(paths.output));
 });
 
@@ -216,46 +197,46 @@ gulp.task('clean:dist', function () {
 // Run unit tests
 gulp.task('test:scripts', function() {
 	return gulp.src([paths.test.input].concat([paths.test.spec]))
-		.pipe(plumber())
-		.pipe(karma({ configFile: paths.test.karma }))
+		.pipe(plg.plumber())
+		.pipe(plg.karma({ configFile: paths.test.karma }))
 		.on('error', function(err) { throw err; });
 });
 
 // Generate documentation
 gulp.task('build:docs', ['compile', 'clean:docs'], function() {
 	return gulp.src(paths.docs.input)
-		.pipe(plumber())
-		.pipe(fileinclude({
+		.pipe(plg.plumber())
+		.pipe(plg.fileInclude({
 			prefix: '@@',
 			basepath: '@file'
 		}))
-		.pipe(tap(function (file, t) {
+		.pipe(plg.tap(function (file, t) {
 			if ( /\.md|\.markdown/.test(file.path) ) {
-				return t.through(markdown);
+				return t.through(plg.markdown);
 			}
 		}))
-		.pipe(header(fs.readFileSync(paths.docs.templates + '/_header.html', 'utf8')))
-		.pipe(footer(fs.readFileSync(paths.docs.templates + '/_footer.html', 'utf8')))
+		.pipe(plg.header(fs.readFileSync(paths.docs.templates + '/_header.html', 'utf8')))
+		.pipe(plg.footer(fs.readFileSync(paths.docs.templates + '/_footer.html', 'utf8')))
 		.pipe(gulp.dest(paths.docs.output));
 });
 
 // Copy distribution files to docs
 gulp.task('copy:dist', ['compile', 'clean:docs'], function() {
 	return gulp.src(paths.output + '/**')
-		.pipe(plumber())
+		.pipe(plg.plumber())
 		.pipe(gulp.dest(paths.docs.output + '/dist'));
 });
 
 // Copy documentation assets to docs
 gulp.task('copy:assets', ['clean:docs'], function() {
 	return gulp.src(paths.docs.assets)
-		.pipe(plumber())
+		.pipe(plg.plumber())
 		.pipe(gulp.dest(paths.docs.output + '/assets'));
 });
 
 gulp.task('build:translations', ['clean:dist'], function() {
 	return gulp.src(paths.translations.input)
-		.pipe(plumber())
+		.pipe(plg.plumber())
 		.pipe(plg.toml({to: JSON.stringify, ext: '.json'}))
 		.pipe(gulp.dest(paths.translations.output));
 });
@@ -279,14 +260,14 @@ var createHtmlPipeline = function (input, output) {
 		.pipe(plg.minifyHtml, {
 			empty: true
 		})
-		.pipe(rename, { suffix: '.min' })
+		.pipe(plg.rename, { suffix: '.min' })
 		.pipe(gulp.dest, output)
 		.pipe(plg.gzip)
 		.pipe(gulp.dest, output);
 
 	return gulp.src(input)
-		.pipe(plumber())
-		.pipe(fileinclude())
+		.pipe(plg.plumber())
+		.pipe(plg.fileInclude())
 		.pipe(gulp.dest(output))
 		.pipe(config.isProduction ? prodPipeline() : plg.util.noop());
 };
@@ -296,15 +277,15 @@ var createJadePipeline = function (input, output) {
 		.pipe(plg.minifyHtml, {
 			empty: true
 		})
-		.pipe(rename, { suffix: '.min' })
+		.pipe(plg.rename, { suffix: '.min' })
 		.pipe(gulp.dest, output)
 		.pipe(plg.gzip)
 		.pipe(gulp.dest, output);
 
 	return gulp.src(input)
-		.pipe(plumber())
+		.pipe(plg.plumber())
 		.pipe(plg.jade())
-		.pipe(fileinclude())
+		.pipe(plg.fileInclude())
 		.pipe(gulp.dest(output))
 		.pipe(config.isProduction ? prodPipeline() : plg.util.noop());
 };
@@ -330,6 +311,21 @@ gulp.task('build:partials-jade', function() {
 	return createJadePipeline(paths.partials.inputJade, paths.partials.output);
 });
 
+/*
+ // Generate documentation
+ gulp.task('docs', [
+ 'clean:docs',
+ 'build:docs',
+ 'copy:dist',
+ 'copy:assets'
+ ]);
+
+ // Generate documentation
+ gulp.task('tests', [
+ 'test:scripts'
+ ]);
+ */
+
 /**
  * Task Runners
  */
@@ -338,7 +334,8 @@ gulp.task('set-production', function () {
 	config.isProduction = true;
 });
 
-var compileTasks = [
+// Compile files
+gulp.task('compile', [
 	'clean:dist',
 	'build:html',
 	'build:jade',
@@ -352,23 +349,7 @@ var compileTasks = [
 	'build:scripts',
 	// 'build:svgs',
 	'build:styles'
-];
-
-// Compile files
-gulp.task('compile', compileTasks);
-
-// Generate documentation
- gulp.task('docs', [
-// 	'clean:docs',
-// 	'build:docs',
-// 	'copy:dist',
-// 	'copy:assets'
 ]);
-
-// Generate documentation
-// gulp.task('tests', [
-// 	'test:scripts'
-// ]);
 
 gulp.task('default', [
 	'compile'
