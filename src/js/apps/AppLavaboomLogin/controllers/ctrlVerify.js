39b54b154689054c0455ab0c4dcd0627cd5c2342
KseniyaKuprina <light.it.zp+kseniya@gmail.com>
2015-01-12 13:47:57 +0200

    fixed verify invite

--- src/js/apps/AppLavaboomLogin/controllers/ctrlVerify.js
+++ src/js/apps/AppLavaboomLogin/controllers/ctrlVerify.js
@@ -1,17 +1,6 @@
 angular.module('AppLavaboomLogin').controller('VerifyController', function($scope, LavaboomAPI) {
 $scope.invite=[];
-console.log("1111");
     $scope.verifyInvite = function() {
-        console.log("1111");
-       /* LavaboomAPI.accounts.reserve.invited({
-            "username": $scope.invite.username,
-            "password": CryptoJS.SHA3($scope.invite.password, {outputLength: 256}).toString(),
-            "token": $scope.invite.token
-        }).then(function (resp) {
-            console.log(resp);
-        }).catch(function (err) {
-            console.log(err);
-        });*/
       LavaboomAPI.accounts.create.invited({
             "username": $scope.invite.username,
             "password": CryptoJS.SHA3($scope.invite.password, {outputLength: 256}).toString(),
@@ -21,5 +10,14 @@ console.log("1111");
         }).catch(function (err) {
             console.log(err);
         });
+   /*   LavaboomAPI.accounts.create.invited({
+            "username": $scope.invite.username,
+            "password": CryptoJS.SHA3($scope.invite.password, {outputLength: 256}).toString(),
+            "token": $scope.invite.token
+        }).then(function (resp) {
+            console.log(resp);
+        }).catch(function (err) {
+            console.log(err);
+        });*/
     };
 });
