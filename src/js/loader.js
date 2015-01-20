5414ad50443f0bcdadd5ecdca8ab6a0caeb6ca68
let4be <let4be@gmail.com>
2015-01-20 10:52:34 +0300

    loader primitives

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,5 +1,31 @@
-var Loader = function () {
+(() => {
+	var Loader = function () {
+		console.log('Initialize loader...');
 
-};
+		var loaderContainer, loaderProgressText, loaderProgressBar, loginAppContainer, mainAppContainer;
 
-window.Loader = new Loader();
+		var hideContainer = (e) => e.className = 'hidden';
+
+		var showContainer = (e) => e.className = '';
+
+		var setProgress = (text, percent) => {
+			loaderProgressText.innerHTML = text;
+			loaderProgressBar.setAttribute('aria-valuenow', percent);
+			loaderProgressBar.setAttribute('style', `width: ${percent}%;`);
+		};
+
+		this.initialize = () => {
+			loaderContainer = document.getElementById('loader-container');
+			loaderProgressText = document.getElementById('loader-progress-text');
+			loaderProgressBar = document.getElementById('loader-progress-bar');
+			loginAppContainer = document.getElementById('login-app-container');
+			mainAppContainer = document.getElementById('main-app-container');
+
+			setProgress('wow', 50);
+			setTimeout(() => hideContainer(loaderContainer), 3000);
+		};
+	};
+
+	window.Loader = new Loader();
+	window.Loader.initialize();
+})();
