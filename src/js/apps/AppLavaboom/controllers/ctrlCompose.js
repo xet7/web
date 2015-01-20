6a7c99c943851d068347a64dfbbc9e1916766e70
Vikram Ramanujam <piggyslasher@gmail.com>
2015-01-20 19:29:25 +0530

    from: dropdown issue - bind to model

--- src/js/apps/AppLavaboom/controllers/ctrlCompose.js
+++ src/js/apps/AppLavaboom/controllers/ctrlCompose.js
@@ -102,5 +102,9 @@ angular.module('AppLavaboom').controller('ComposeController', function($scope, L
 
 	  $scope.htmlVariable = '<h2>Try me!</h2><p>textAngular is a super cool WYSIWYG Text Editor directive for AngularJS</p><p><b>Features:</b></p><ol><li>Automatic Seamless Two-Way-Binding</li><li style="color: blue;">Super Easy <b>Theming</b> Options</li><li>Simple Editor Instance Creation</li><li>Safely Parses Html for Custom Toolbar Icons</li><li>Doesn\'t Use an iFrame</li><li>Works with Firefox, Chrome, and IE8+</li></ol><p><b>Code at GitHub:</b> <a href="https://github.com/fraywing/textAngular">Here</a> </p>';
 
+
+	  $scope.fromEmails = [{mail: "piggyslasher@lavaboom.com"}, {mail:"bubba@shrimp.com"}, {name:"wholet@dog.out"}]
+	  $scope.fromEmails.selected = [{mail: "piggyslasher@lavaboom.com"}];
+
 	
 });
