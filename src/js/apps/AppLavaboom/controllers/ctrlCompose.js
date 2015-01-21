9701b55ff3aae2adb24a58e8ef6de13de5753c3f
let4be <let4be@gmail.com>
2015-01-21 11:24:38 +0300

    fixes for feature/compose merge, added /cache to .gitignore

--- src/js/apps/AppLavaboom/controllers/ctrlCompose.js
+++ src/js/apps/AppLavaboom/controllers/ctrlCompose.js
@@ -1,41 +1,4 @@
-/**
- * AngularJS default filter with the following expression:
- * "person in people | filter: {name: $select.search, sec: $select.search}"
- * performs a AND between 'name: $select.search' and 'sec: $select.search'.
- * We want to perform a OR.
- */
-window.app.filter('propsFilter', function() {
-  return function(items, props) {
-    var out = [];
-
-    if (angular.isArray(items)) {
-      items.forEach(function(item) {
-        var itemMatches = false;
-
-        var keys = Object.keys(props);
-        for (var i = 0; i < keys.length; i++) {
-          var prop = keys[i];
-          var text = props[prop].toLowerCase();
-          if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
-            itemMatches = true;
-            break;
-          }
-        }
-
-        if (itemMatches) {
-          out.push(item);
-        }
-      });
-    } else {
-      // Let the output be the input untouched
-      out = items;
-    }
-
-    return out;
-  };
-});
-
-angular.module('AppLavaboom').controller('ComposeController', function($scope, LavaboomAPI) {
+angular.module('AppLavaboom').controller('CtrlCompose', function($scope) {
 
       $scope.disabled = undefined;
 	  $scope.searchEnabled = undefined;
