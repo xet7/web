4eaf89a249c1c2d6cdbe8a8c2a12959708acb5ae
Vikram Ramanujam <piggyslasher@gmail.com>
2015-01-22 21:41:52 +0530

    signature - just simple stuff for the text angular in controller

--- src/js/apps/AppLavaboom/controllers/ctrlSettingsPersonal.js
+++ src/js/apps/AppLavaboom/controllers/ctrlSettingsPersonal.js
@@ -4,4 +4,6 @@ angular.module(primaryApplicationName).controller('ctrlSettingsPersonal', functi
 	console.log('$scope.username', $scope.username);
 
 	$scope.settings = user.settings;
+
+	$scope.signature='<img src="https://mail.lavaboom.io/img/Lavaboom-logo.svg" style="float: left;"/><p><br>Felix von Looz<br>Head of Design, Lavaboom<br><a href="mailto:fvl@lavaboom.com">fvl@lavaboom.com</a><br><br>P.s. <em>Still reading?</em> Hurry and get your secure email at lavaboom.com</p>';
 });
