5b0ddba6beb8767c17c87e5b976ead36754169b7
let4be <let4be@gmail.com>
2015-01-26 17:06:48 +0300

    inbox: "loading..." when switching between threads

--- src/js/apps/AppLavaboom/controllers/ctrlMailDetail.js
+++ src/js/apps/AppLavaboom/controllers/ctrlMailDetail.js
@@ -1,15 +1,21 @@
 angular.module(primaryApplicationName).controller('CtrlMailDetail', function($scope, inbox) {
+	$scope.isLoading = false;
 	$scope.selected = null;
 	$scope.emails = [];
 
 	$scope.$on('inbox-selection-changed', (e, selected) => {
-		console.log(selected);
 		$scope.selected = selected;
 
-		if (selected)
+		if (selected) {
+			$scope.isLoading = true;
+			$scope.emails = [];
 			inbox.getEmailsByThreadId(selected.id)
 				.then(emails => {
 					$scope.emails = emails;
+				})
+				.finally(() => {
+					$scope.isLoading = false;
 				});
+		}
 	});
 });
