7cd68f0ba04e9c3a7d365e98bb78da347183f2a1
Piotr Zduniak <piotr@zduniak.net>
2015-02-02 18:15:30 +0100

    Added proper dist transfer

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,5 @@
 import os, random, string
-from fabric.api import run, env, cd, settings, put
+from fabric.api import run, env, cd, settings, put, local
 
 env.key_filename = os.getenv('HOME', '/root') + '/.ssh/id_rsa'
 
@@ -8,12 +8,15 @@ def deploy():
 	commit = os.getenv('DRONE_COMMIT', 'master')
 	tmp_dir = '/tmp/' + ''.join(random.choice(string.lowercase) for i in xrange(10))
 
+	local('tar cvfz dist.tgz dist/')
 	run('mkdir ' + tmp_dir)
 	with cd(tmp_dir):
-		run('mkdir ' + tmp_dir + '/web')
-		put('dist', tmp_dir + '/web/dist')
+		run('mkdir -p ' + tmp_dir + '/web/dist')
+		put('dist.tgz', tmp_dir + '/web/dist.tgz')
 		put('Dockerfile', tmp_dir + '/web/Dockerfile')
+
 		with cd('web'):
+			run('tar -xzvf dist.tgz')
 			run('docker build -t registry.lavaboom.io/lavaboom/web-' + branch + ' .')
 
 		run('git clone git@github.com:lavab/docker.git')
