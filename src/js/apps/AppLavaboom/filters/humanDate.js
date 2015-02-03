91a1189b28966027d3d6600af3da532753f0dbef
let4be <let4be@gmail.com>
2015-02-03 23:20:25 +0300

    added humanDate filter(i18n, today, yesterday)

---
+++ src/js/apps/AppLavaboom/filters/humanDate.js
@@ -0,0 +1,16 @@
+angular.module(primaryApplicationName).filter('humanDate', (dateFilter, translateFilter) => {
+	return (date) => {
+		var now = new Date();
+		var dateObj = new Date(date);
+
+		if (dateObj.toDateString() == now.toDateString())
+			return translateFilter('DATES.TODAY');
+
+		var yesterday = new Date(new Date().setDate(new Date().getDate()-1));
+
+		if (dateObj.toDateString() == yesterday.toDateString())
+			return translateFilter('DATES.YESTERDAY');
+
+		return dateFilter(dateObj);
+	};
+});
