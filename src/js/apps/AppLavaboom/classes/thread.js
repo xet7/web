82fc80b0403778575915ddb490631cb5c86e52ff
let4be <let4be@gmail.com>
2015-02-04 15:34:41 +0300

    angular: new members filter

--- src/js/apps/AppLavaboom/classes/thread.js
+++ src/js/apps/AppLavaboom/classes/thread.js
@@ -14,19 +14,6 @@ angular.module(primaryApplicationName).factory('Thread', function($rootScope, co
 		this.modified = opt.date_modified;
 		this.members = opt.members;
 
-		var members = opt.members.map(email => {
-			var contact = contacts.getContactByEmail(email);
-			return contact ? contact.name : email;
-		});
-
-		this.membersString = members.slice(0, 2).join(',');
-		if (members.length == 3)
-			this.membersString += ' ' + translations.LB_AND_ONE_OTHER;
-		else if (members.length == 4)
-			this.membersString += ' ' + translations.LB_AND_TWO_OTHERS;
-		else if (members.length > 4)
-			this.membersString += ' ' + translations.LB_AND_OTHERS;
-
 		this.isRead = opt.is_read;
 		this.isEncrypted = true;
 		this.attachmentsCount = 0;
