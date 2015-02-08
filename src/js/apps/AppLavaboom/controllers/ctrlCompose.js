bcfe7c3338bf57fd2047da56a19f7e6df859d61d
let4be <let4be@gmail.com>
2015-02-08 10:57:03 +0300

    small compose screen improvements

--- src/js/apps/AppLavaboom/controllers/ctrlCompose.js
+++ src/js/apps/AppLavaboom/controllers/ctrlCompose.js
@@ -143,6 +143,7 @@ angular.module('AppLavaboom').controller('CtrlCompose', function($scope, $stateP
 				subject: 'Test subject',
 				body: '<p>Dear Orwell</p><p>Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Sed porttitor lectus nibh. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Donec sollicitudin molestie malesuada. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Donec rutrum congue leo eget malesuada. Sed porttitor lectus nibh. Curabitur aliquet quam id dui posuere blandit. Nulla porttitor accumsan tincidunt.</p><blockquote><p>See, there never was actually any spoon. It was just lying around the production set.</p></blockquote>'
 			};
+
 			bindUserSignature();
 		}
 	});
@@ -150,6 +151,8 @@ angular.module('AppLavaboom').controller('CtrlCompose', function($scope, $stateP
 	$scope.clearTo = () => $scope.form.selected.to = [];
 	$scope.clearCC = () => $scope.form.selected.cc = [];
 	$scope.clearBCC = () => $scope.form.selected.bcc = [];
+
+	$scope.taggingTokens = 'SPACE|,|/';
 
 	$scope.tagTransform = function (newTag) {
 		var p = newTag.split('@');
@@ -165,13 +168,5 @@ angular.module('AppLavaboom').controller('CtrlCompose', function($scope, $stateP
 			email: `${newTag.trim()}@${consts.ROOT_DOMAIN}`,
 			sec: 1
 		};
-	};
-
-	$scope.enable = function() {
-		$scope.disabled = false;
-	};
-
-	$scope.disable = function() {
-		$scope.disabled = true;
 	};
 });
