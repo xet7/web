88ec9507aceac3f3005aaff1c937e663e0637ee2
let4be <let4be@gmail.com>
2015-02-11 14:20:57 +0300

    moar brains

--- src/js/apps/AppLavaboom/controllers/ctrlContactList.js
+++ src/js/apps/AppLavaboom/controllers/ctrlContactList.js
@@ -1,10 +1,25 @@
-angular.module(primaryApplicationName).controller('CtrlContactList', function($rootScope, $scope, $state, $stateParams, co, contacts, Contact) {
+angular.module(primaryApplicationName).controller('CtrlContactList', function($rootScope, $scope, $translate, $state, $stateParams, co, contacts, Contact) {
 	$scope.selectedContactId = null;
 	$scope.searchText = '';
 
+	var translations = {};
+
+	$rootScope.$bind('$translateChangeSuccess', () => {
+		translations.LB_NEW_CONTACT_SHORT = $translate.instant('MAIN.CONTACTS.LB_NEW_CONTACT');
+		translations.LB_EMPTY_CONTACT_SHORT = $translate.instant('MAIN.CONTACTS.LB_EMPTY_CONTACT');
+	});
+
 	$rootScope.whenInitialized(() => {
 		$scope.$bind('contacts-changed', () => {
-			$scope.people = _.groupBy(contacts.peopleList, contact => contact.name[0]);
+			$scope.people = _.groupBy(contacts.peopleList, contact => {
+				if (contact.isNew)
+					return '+';
+
+				if (!contact.name)
+					return '?';
+
+				return contact.name[0];
+			});
 			$scope.letters = _.sortBy(Object.keys($scope.people), letter => letter);
 		});
 
