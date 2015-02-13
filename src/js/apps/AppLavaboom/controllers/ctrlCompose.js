190726eebed6ebd4b9d05454d5be15f6a0b33dab
let4be <let4be@gmail.com>
2015-02-13 11:37:46 +0300

    removed compose screen test message

--- src/js/apps/AppLavaboom/controllers/ctrlCompose.js
+++ src/js/apps/AppLavaboom/controllers/ctrlCompose.js
@@ -175,7 +175,7 @@ angular.module('AppLavaboom').controller('CtrlCompose', function($rootScope, $sc
 				},
 				fromEmails: [contacts.myself],
 				subject: 'Test subject',
-				body: '<p>Dear Orwell</p><p>Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Sed porttitor lectus nibh. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Donec sollicitudin molestie malesuada. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Donec rutrum congue leo eget malesuada. Sed porttitor lectus nibh. Curabitur aliquet quam id dui posuere blandit. Nulla porttitor accumsan tincidunt.</p><blockquote><p>See, there never was actually any spoon. It was just lying around the production set.</p></blockquote><a href="mailto:aerials@soad.com">test mailto link</a>'
+				body: ''
 			};
 
 			bindUserSignature();
