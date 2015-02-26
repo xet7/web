e697f62aebae5b181505847ba688c9e611a96dc5
Sebastian Larsson <sebastian-larsson@outlook.com>
2015-02-26 14:29:30 +0100

    Hotkeys - Navigation Hotkeys on Settings Page

--- src/js/apps/AppLavaboom/factories/hotkey.js
+++ src/js/apps/AppLavaboom/factories/hotkey.js
@@ -154,5 +154,57 @@ module.exports = /*@ngInject*/($translate, hotkeys, $rootScope, $state) => {
         console.log('Hotkeys', 'Added global hotkeys');
     };
 
+    Hotkey.addSettingsNavigationHotkeys = () => {
+        Hotkey.addHotkey({
+            combo: ['h', 'k', 'left', 'up'],
+            description: 'HOTKEY.MOVE_UP',
+            callback: (event, key) => {
+                event.preventDefault();
+                switch ($state.current.name) {
+                    case 'main.settings.general':
+                        $state.go('main.settings.plan');
+                        break;
+                    case 'main.settings.profile':
+                        $state.go('main.settings.general');
+                        break;
+                    case 'main.settings.security':
+                        $state.go('main.settings.profile');
+                        break;
+                    case 'main.settings.plan':
+                        $state.go('main.settings.security');
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+
+        Hotkey.addHotkey({
+            combo: ['j', 'l', 'right', 'down'],
+            description: 'HOTKEY.MOVE_DOWN',
+            callback: (event, key) => {
+                event.preventDefault();
+                switch ($state.current.name) {
+                    case 'main.settings.general':
+                        $state.go('main.settings.profile');
+                        break;
+                    case 'main.settings.profile':
+                        $state.go('main.settings.security');
+                        break;
+                    case 'main.settings.security':
+                        $state.go('main.settings.plan');
+                        break;
+                    case 'main.settings.plan':
+                        $state.go('main.settings.general');
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+
+        console.log('Hotkeys', 'Added settings navigation hotkeys');
+    };
+
     return Hotkey;
 };
