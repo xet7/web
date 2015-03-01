cf3fa15be12372ca17dff8c69b05859979721ab7
let4be <let4be@gmail.com>
2015-03-01 12:02:34 +0300

    cache.js: cache of values by key + dependant cache(list unfold)

--- src/js/apps/AppLavaboom/factories/cache.js
+++ src/js/apps/AppLavaboom/factories/cache.js
@@ -1,20 +1,21 @@
 module.exports = /*@ngInject*/(co) => {
-	var Cache = function (opts = {}) {
-		var cache = {};
+	const Cache = function (opts = {}) {
+		let cacheByKey = {};
+		let cacheById = {};
 		const self = this;
 
-		this.get = (name) => {
-			var r = cache[name];
+		if (!opts.unfold)
+			opts.unfold = null;
+		
+		this.get = (key) => {
+			const r = cacheByKey[key];
 			if (!r)
 				return null;
 
-			var now = new Date();
+			const now = new Date();
 
 			if (now - r.time > opts.ttl) {
-				if (opts.isInvalidateWholeCache)
-					self.invalidateAll();
-				else
-					self.invalidate(name);
+				self.invalidate(key);
 
 				return null;
 			}
@@ -22,26 +23,63 @@ module.exports = /*@ngInject*/(co) => {
 			return r.value;
 		};
 
-		this.put = (name, value) => {
-			cache[name] = {
-				value: value,
-				time: new Date()
-			};
+		this.getById = (id) => {
+			const r = cacheById[id];
+			if (!r)
+				return null;
+
+			const rList = cacheByKey[r.key];
+
+			const now = new Date();
+
+			if (now - rList.time > opts.ttl) {
+				self.invalidate(r.key);
+
+				return null;
+			}
+
+			return r.value;
 		};
 
-		this.invalidate = (name) => {
-			if (cache[name])
-				delete cache[name];
+		this.put = (key, value) => {
+			const date = new Date();
+
+			cacheByKey[key] = {
+				value: value,
+				time: date
+			};
+			if (opts.unfold)
+				value.forEach(item => {
+					const id = opts.unfold(item);
+					cacheById[id] = {
+						value: item,
+						key: key,
+						time: date
+					};
+				});
+		};
+
+		this.invalidate = (key) => {
+			if (cacheByKey[key]) {
+				const value = cacheByKey[key];
+				if (opts.unfold)
+					value.forEach(item => {
+						const id = opts.unfold(item);
+						delete cacheById[id];
+					});
+				delete cacheByKey[key];
+			}
 		};
 
 		this.invalidateAll = () => {
-			cache = {};
+			cacheByKey = {};
+			cacheById = {};
 		};
 
 		this.call = (promiseInvoker, args) => co(function* (){
-			var key = JSON.stringify(args);
+			let key = JSON.stringify(args);
 
-			var r = self.get(key);
+			let r = self.get(key);
 			if (r)
 				return r;
 
