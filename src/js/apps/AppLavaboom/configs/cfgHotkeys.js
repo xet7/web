d2cb656f1c114fade49476aed61d6068db2b2333
Sebastian Larsson <sebastian-larsson@outlook.com>
2015-03-03 12:36:42 +0100

    hotkeys screen - using dl, dt, label, label-default

--- src/js/apps/AppLavaboom/configs/cfgHotkeys.js
+++ src/js/apps/AppLavaboom/configs/cfgHotkeys.js
@@ -2,14 +2,12 @@ module.exports = /*@ngInject*/(hotkeysProvider) => {
     hotkeysProvider.templateTitle = 'Keyboard shortcuts';
     hotkeysProvider.template =  '<div class="cfp-hotkeys-container fade" ng-class="{in: helpVisible}" style="display: none;"><div class="cfp-hotkeys">' +
                                     '<h4 class="cfp-hotkeys-title">{{ title }}</h4>' +
-                                    '<table><tbody>' +
-                                        '<tr ng-repeat="hotkey in hotkeys | filter:{ description: \'!$$undefined$$\' }">' +
-                                            '<td class="cfp-hotkeys-keys">' +
-                                                '<span ng-repeat="key in hotkey.combo track by $index" class="cfp-hotkeys-key">{{ key }}</span>' +
-                                            '</td>' +
-                                            '<td class="cfp-hotkeys-text">{{ hotkey.description }}</td>' +
-                                        '</tr>' +
-                                    '</tbody></table>' +
+                                    '<dl ng-repeat="hotkey in hotkeys | filter:{ description: \'!$$undefined$$\' }">' +
+                                        '<dt class="cfp-hotkeys-keys">' +
+                                        '<span ng-repeat="key in hotkey.combo track by $index" class="cfp-hotkeys-key label label-default">{{ key }}</span>' +
+                                        '</dt>' +
+                                        '<dd class="cfp-hotkeys-text">{{ hotkey.description }}</dd>' +
+                                    '</dl>' +
                                     '<div class="cfp-hotkeys-close" ng-click="toggleCheatSheet()">×</div>' +
                                 '</div></div>';
     hotkeysProvider.cheatSheetDescription = 'Show/Hide Keyboard Shortcuts';
