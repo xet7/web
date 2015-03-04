4a70859fa9dcc7680d2a09299c580e19d4680334
let4be <let4be@gmail.com>
2015-03-04 13:48:03 +0300

    angular-hotkeys was forked(added templateUrl support), hotkeys template moved to jade

--- src/js/apps/AppLavaboom/configs/cfgHotkeys.js
+++ src/js/apps/AppLavaboom/configs/cfgHotkeys.js
@@ -1,14 +1,5 @@
 module.exports = /*@ngInject*/(hotkeysProvider) => {
     hotkeysProvider.templateTitle = 'Keyboard shortcuts';
-    hotkeysProvider.template =  '<div class="cfp-hotkeys-container fade" ng-class="{in: helpVisible}" style="display: none;"><div class="cfp-hotkeys">' +
-                                    '<h4 class="cfp-hotkeys-title">{{ title }}</h4>' +
-                                    '<dl ng-repeat="hotkey in hotkeys | filter:{ description: \'!$$undefined$$\' }">' +
-                                        '<dt class="cfp-hotkeys-keys">' +
-                                        '<span ng-repeat="key in hotkey.combo track by $index" class="cfp-hotkeys-key label label-default">{{ key }}</span>' +
-                                        '</dt>' +
-                                        '<dd class="cfp-hotkeys-text">{{ hotkey.description }}</dd>' +
-                                    '</dl>' +
-                                    '<div class="cfp-hotkeys-close" ng-click="toggleCheatSheet()">×</div>' +
-                                '</div></div>';
+    hotkeysProvider.templateUrl = 'partials/hotkeys.html';
     hotkeysProvider.cheatSheetDescription = 'Show/Hide Keyboard Shortcuts';
 };
