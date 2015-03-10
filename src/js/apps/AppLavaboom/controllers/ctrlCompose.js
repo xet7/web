3dd78fc086d513a4a3db86ad5e448f0ff572446a
let4be <let4be@gmail.com>
2015-03-10 12:13:27 +0300

    fixed bug with keys gathering upon emails send(unencrypted emails)

--- src/js/apps/AppLavaboom/controllers/ctrlCompose.js
+++ src/js/apps/AppLavaboom/controllers/ctrlCompose.js
@@ -148,7 +148,7 @@ module.exports = /*@ngInject*/($rootScope, $scope, $stateParams, $translate,
 			bcc = $scope.form.selected.bcc.map(e => e.email);
 
 		let keys = yield ([...$scope.form.selected.to, ...$scope.form.selected.cc, ...$scope.form.selected.bcc].reduce((a, e) => {
-			a[e.email] = co.transform(co.def(e.loadKey(), null), v => v.key);
+			a[e.email] = co.transform(co.def(e.loadKey(), null), v => v ? v.key : v);
 			return a;
 		}, {}));
 
