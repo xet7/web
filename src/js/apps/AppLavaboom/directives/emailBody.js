b947086117aba4a60d39834965b0ec8fc349328d
let4be <let4be@gmail.com>
2015-03-11 10:39:13 +0300

    improved email-body directive to handle external images in a safe way, now template is being binded only after processing html by email-body

--- src/js/apps/AppLavaboom/directives/emailBody.js
+++ src/js/apps/AppLavaboom/directives/emailBody.js
@@ -1,15 +1,29 @@
-module.exports = /*@ngInject*/($timeout, $state) => {
+module.exports = /*@ngInject*/($timeout, $state, $compile, $sanitize, user) => {
 	return {
 		restrict : 'A',
+		scope: {
+			emailBody: '='
+		},
 		link  : (scope, el, attrs) => {
 			$timeout(() => {
-				angular.forEach(el.find('a'), e => {
+				const emailBodyHtml = angular.element('<div>' + $sanitize(scope.emailBody) + '</div>');
+
+				angular.forEach(emailBodyHtml.find('a'), e => {
 					e = angular.element(e);
-					if (e.attr('href').indexOf('mailto:') === 0) {
+					if (e.attr('href').startsWith('mailto:')) {
 						e.attr('href', $state.href('.popup.compose', {to: e.attr('href').replace('mailto:', '').trim()}));
 					} else
 						e.attr('target', '_blank');
 				});
+
+				if (user.settings.isSecuredImages)
+					angular.forEach(emailBodyHtml.find('img'), e => {
+						e = angular.element(e);
+						if (!e.attr('src').startsWith('data:'))
+							e.attr('src', '/img/no-image.png');
+					});
+
+				el.append(emailBodyHtml);
 			});
 		}
 	};
