655c215ba9aa080b710308e6484d1b84bea0409b
let4be <let4be@gmail.com>
2015-03-12 17:24:11 +0300

    fixed #276

--- src/js/apps/AppLavaboom/directives/customDropZone.js
+++ src/js/apps/AppLavaboom/directives/customDropZone.js
@@ -1,9 +1,21 @@
 module.exports = /*@ngInject*/($timeout, $state) => {
 	return {
 		restrict : 'A',
-		link: function (scope, elem) {
-			window.addEventListener('dragover', e => e.preventDefault(), false);
-			window.addEventListener('drop', e => e.preventDefault(), false);
+		link:  (scope, elem) => {
+			window.addEventListener('dragover', e => {
+				console.log('drop event', e);
+				if (!e.target.id.startsWith('taTextElement')) {
+					console.log('drop event preventDefault');
+					e.preventDefault();
+				}
+			}, false);
+			window.addEventListener('drop', e => {
+				console.log('drop event', e);
+				if (!e.target.id.startsWith('taTextElement')) {
+					console.log('drop event preventDefault');
+					e.preventDefault();
+				}
+			}, false);
 		}
 	};
 };
