4cbe5d4b8a80b53c3a7bf19eac024562b2b4fb24
let4be <let4be@gmail.com>
2015-03-27 13:37:51 +0300

    login: "remember me" removed and now is always the same as "this is private pc", closed #349

--- src/js/apps/AppLavaboomLogin/controllers/ctrlAuth.js
+++ src/js/apps/AppLavaboomLogin/controllers/ctrlAuth.js
@@ -10,19 +10,17 @@ module.exports = /*@ngInject*/($scope, $rootScope, $window, $interval, co, user,
 	$scope.form = {
 		username: '',
 		password: '',
-		isRemember: signInSettings ? signInSettings.isRemember : false,
 		isPrivateComputer: signInSettings ? signInSettings.isPrivateComputer : false
 	};
 
 	$scope.$watch('form', (o, n) => {
 		localStorage.signInSettings = JSON.stringify({
-			isRemember: $scope.form.isRemember,
 			isPrivateComputer: $scope.form.isPrivateComputer
 		});
 	}, true);
 
 	$scope.logIn = () => co(function *(){
-		yield user.signIn($scope.form.username, $scope.form.password, $scope.form.isRemember, $scope.form.isPrivateComputer);
+		yield user.signIn($scope.form.username, $scope.form.password, $scope.form.isPrivateComputer, $scope.form.isPrivateComputer);
 
 		loader.resetProgress();
 		loader.showLoader(true);
