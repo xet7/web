ba14b1cf622ac4fec5f159458e1705072eb20ebc
let4be <let4be@gmail.com>
2015-04-16 11:46:46 +0300

    closed #486, added custom tagOnFocusLoss directive to asap ui-select with tagging on focus loss

---
+++ src/js/apps/AppLavaboom/directives/tagOnFocusLoss.js
@@ -0,0 +1,36 @@
+module.exports = /*@ngInject*/ ($parse, $timeout) => {
+	return {
+		restrict: 'A',
+		require: 'ngModel',
+		link: function (scope, element, attrs, ngModel) {
+			var toggleInput = element.find('input')[0];
+			toggleInput.addEventListener('blur', (event) => {
+				let searchValue = $parse(attrs.tagOnFocusLoss)(scope);
+				if (searchValue)
+					searchValue = searchValue.trim();
+
+				if (searchValue) {
+					const tagTransform = $parse(attrs.tagging)(scope);
+					
+					$timeout(() => {
+						const target = angular.element(event.explicitOriginalTarget || document.activeElement);
+						const isTransferable = (target, lvl = 1) => {
+							if (target.hasClass('tag-transferable'))
+								return true;
+
+							if (target.parent() && lvl < 3)
+								return isTransferable(angular.element(target.parent()), lvl + 1);
+
+							return false;
+						};
+
+						if (isTransferable(target)) {
+							const tag = tagTransform(searchValue);
+							ngModel.$modelValue.push(tag);
+						}
+					});
+				}
+			});
+		}
+	};
+};
