5eb056fe9a4e40e5fd52c188b607717ccf51276f
let4be <let4be@gmail.com>
2015-04-17 11:33:52 +0300

    mmm

--- src/js/apps/AppLavaboom/directives/textAngularFocus.js
+++ src/js/apps/AppLavaboom/directives/textAngularFocus.js
@@ -2,7 +2,7 @@ module.exports = /*@ngInject*/ ($parse, $timeout, textAngularManager) => {
 	return {
 		link: (scope, element, attributes) => {
 			$timeout(() => {
-				var editorScope = textAngularManager.retrieveEditor(attributes.name).scope;
+				let editorScope = textAngularManager.retrieveEditor(attributes.name).scope;
 				editorScope.displayElements.text.triggerHandler('focus');
 				editorScope.displayElements.text[0].focus();
 			});
