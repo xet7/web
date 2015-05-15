29af2076812e438f07b734e7cd4db59295f66a06
let4be <let4be@gmail.com>
2015-05-15 10:31:07 +0300

    updated keyring backup manual link, closed #652

--- src/js/apps/Utils/constants/consts.js
+++ src/js/apps/Utils/constants/consts.js
@@ -32,7 +32,7 @@ module.exports = {
 
 	INBOX_EMAILS_CACHE_TTL: 60 * 10 * 1000,
 	SET_READ_AFTER_TIMEOUT: 3000,
-	KEYS_BACKUP_README: 'https://lavaboom.com/faq/keyring-backup',
+	KEYS_BACKUP_README: 'https://lavab.groovehq.com/knowledge_base/topics/backing-up-your-keyring',
 	POPUP_AUTO_HIDE_DELAY: 500,
 	ORDERED_LABELS: ['Inbox', 'Drafts', 'Sent', 'Starred', 'Spam', 'Trash'],
 	PLAN_LIST: ['BASIC'/*, 'TEST'*/],
