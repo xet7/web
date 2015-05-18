947afada6fdeb8613a666926b3d3c024ca098329
Eugene Krevenets <hyzhak@ya.ru>
2015-05-18 18:58:55 +0200

    remove useless comments

--- karma-integration.conf.js
+++ karma-integration.conf.js
@@ -1,23 +1,12 @@
-// Karma configuration
-// Generated on Wed May 06 2015 20:03:29 GMT+0200 (CEST)
-
 module.exports = function (config) {
 	config.set({
-
-		// base path that will be used to resolve all patterns (eg. files, exclude)
 		basePath: '',
-
-
-		// frameworks to use
-		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
 
 		// (!) we can’t use babel preprocessor (https://github.com/babel/karma-babel-preprocessor) here
 		// because of https://github.com/nikku/karma-browserify#transforms
 		// and use https://github.com/babel/babelify below
 		frameworks: ['browserify', 'jasmine'],
 
-
-		// list of files / patterns to load in the browser
 		files: [
 			'dist/js/utils-vendor.js',
 			'dist/js/appLavaboom-vendor.js',
@@ -42,13 +31,8 @@ module.exports = function (config) {
 			'/vendor': 'http://localhost:9876/base/dist/vendor'
 		},
 
-
-		// list of files to exclude
 		exclude: [],
 
-
-		// preprocess matching files before serving them to the browser
-		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
 		preprocessors: {
 			'src/tests/**/*Spec.js': ['browserify']
 		},
@@ -59,37 +43,18 @@ module.exports = function (config) {
 			transform: ['babelify', 'envify', 'brfs']
 		},
 
-
-		// test results reporter to use
-		// possible values: 'dots', 'progress'
-		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
 		reporters: ['progress'],
 
-
-		// web server port
 		port: 9876,
 
-
-		// enable / disable colors in the output (reporters and logs)
 		colors: true,
 
-
-		// level of logging
-		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
 		logLevel: config.LOG_INFO,
 
-
-		// enable / disable watching file and executing tests whenever any file changes
 		autoWatch: true,
 
-
-		// start these browsers
-		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
 		browsers: ['PhantomJS'],
 
-
-		// Continuous Integration mode
-		// if true, Karma captures browsers, runs the tests and exits
 		singleRun: true,
 
 		browserNoActivityTimeout: 60000
