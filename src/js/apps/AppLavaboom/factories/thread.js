5981071c769d269867afc33e12e652497ac16467
let4be <let4be@gmail.com>
2015-05-19 15:04:56 +0300

    better error handling/tolerating in thread.fromEnvelope

--- src/js/apps/AppLavaboom/factories/thread.js
+++ src/js/apps/AppLavaboom/factories/thread.js
@@ -7,7 +7,7 @@ module.exports = /*@ngInject*/($injector, $translate, co, utils, crypto, user, E
 	function Thread(opt, manifest, labels) {
 		const self = this;
 		let isLoaded = false;
-		
+
 		const prettify = (a) => {
 			let r = a
 				.map(e => e.address == user.email ? '' : e.prettyName)
@@ -36,30 +36,29 @@ module.exports = /*@ngInject*/($injector, $translate, co, utils, crypto, user, E
 
 			return members;
 		};
-		
+
+		self.id = opt.id;
+		self.created = opt.date_created;
+		self.modified = opt.date_modified;
+
+		self.members = opt.members && opt.members.length > 0 ? filterMembers(Manifest.parseAddresses(opt.members)) : [];
+		self.membersPretty = prettify(self.members);
+		self.labels = opt.labels && opt.labels.length > 0 ? opt.labels : [];
+		self.isRead = !!opt.is_read;
+		self.secure = opt.secure;
+		self.isReplied = opt.emails.length > 1;
+		self.isForwarded = Email.getSubjectWithoutRe(self.subject) != self.subject;
+
 		this.setupManifest = (manifest, setIsLoaded = false) => {
 			isLoaded = setIsLoaded;
-			self.id = opt.id;
-			self.created = opt.date_created;
-			self.modified = opt.date_modified;
-
-			self.members = opt.members ? filterMembers(Manifest.parseAddresses(opt.members)) : [];
-			self.membersPretty = prettify(self.members);
 
 			self.to = manifest ? manifest.to : [];
-
-			self.labels = opt.labels;
-			self.isRead = opt.is_read;
-			self.secure = opt.secure;
 
 			self.subject = manifest && manifest.subject ? manifest.subject : opt.subject;
 			if (!self.subject)
 				self.subject = '';
 
 			self.attachmentsCount = manifest && manifest.files ? manifest.files.length : 0;
-
-			self.isReplied = opt.emails.length > 1;
-			self.isForwarded = Email.getSubjectWithoutRe(self.subject) != self.subject;
 		};
 		
 		this.isLoaded = () => isLoaded;
@@ -77,8 +76,14 @@ module.exports = /*@ngInject*/($injector, $translate, co, utils, crypto, user, E
 		let thread = new Thread(envelope, null, labels);
 
 		co(function *(){
-			let manifestRaw = yield co.def(crypto.decodeRaw(envelope.manifest), null);
-			console.log('thread manifest', manifestRaw);
+			let manifestRaw;
+			try {
+				manifestRaw = yield co.def(crypto.decodeRaw(envelope.manifest), null);
+				console.log('thread manifest', manifestRaw);
+			} catch (err) {
+				thread.setupManifest(null, true);
+				throw err;
+			}
 			thread.setupManifest(manifestRaw ? Manifest.createFromJson(manifestRaw) : null, true);
 		});
 
